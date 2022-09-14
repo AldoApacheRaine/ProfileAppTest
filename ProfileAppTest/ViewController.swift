79e567b75c2d27389aa957002004bb03ea9242dd
Max Hmelev <mha93@yandex.ru>
2022-09-14 19:51:41 +0300

    Initial Commit

---
+++ ProfileAppTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ProfileAppTest
+//
+//  Created by Максим Хмелев on 14.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
